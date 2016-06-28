79ccd482fdcc303de0b405f28c3c30c7fbd582ec
zzy9206 <zzy9206@126.com>
2016-06-28 13:54:40 +0800

    zzy

---
+++ app/page/me/setting.js
@@ -0,0 +1,29 @@
+/**
+ * Created by myzh on 2016/6/28.
+ */
+'use strict';
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class setting extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text >
+                    设置界面
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+});
+
